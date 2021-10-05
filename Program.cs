75e76dac4df6c2ce0ec3939c3fa139a536d0b364
Noriaki123 <ashatamega@gmail.com>
2021-10-05 21:39:20 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
