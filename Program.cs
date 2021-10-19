2fb74178d7030e6497e6df2e56f322dfd23e2e4a
PryseOlg <oleg.kuts.02@mail.ru>
2021-10-19 22:28:16 +0500

    Added TASK6

--- Program.cs
+++ Program.cs
@@ -58,10 +58,153 @@ namespace alg2 {
                 }
             }
     }
+     class GFG {
+         internal class Node {
+             public int data;
+             public Node left, right;
+         };
 
-    class Program {
-        static void Main(string[] args) {
-            //Backtracking.DoTask2();
-        }
-    }
+/* This function is here just to test buildTree() */
+public static void printInorder(Node node) {
+             if (node == null)
+                 return;
+             printInorder(node.left);
+             Console.Write(node.data + " ");
+             printInorder(node.right);
+         }
+
+// Recursively consubtree under given root using
+// leftChil[] and rightchild
+          public static Node buildCartesianTreeUtil(int root, int[] arr,
+             int[] parent, int[] leftchild, int[] rightchild) {
+             if (root == -1)
+                 return null;
+
+             // Create a new node with root's data
+             Node temp = new Node();
+             temp.data = arr[root];
+
+             // Recursively conleft and right subtrees
+             temp.left = buildCartesianTreeUtil(leftchild[root],
+                 arr, parent, leftchild, rightchild);
+             temp.right = buildCartesianTreeUtil(rightchild[root],
+                 arr, parent, leftchild, rightchild);
+
+             return temp;
+         }
+
+// A function to create the Cartesian Tree in O(N) time
+public static Node buildCartesianTree(int[] arr, int n) {
+             // Arrays to hold the index of parent, left-child,
+             // right-child of each number in the input array
+             int[] parent = new int[n];
+             int[] leftchild = new int[n];
+             int[] rightchild = new int[n];
+
+             // Initialize all array values as -1
+             memset(parent, -1);
+             memset(leftchild, -1);
+             memset(rightchild, -1);
+
+             // 'root' and 'last' stores the index of the root and the
+             // last processed of the Cartesian Tree.
+             // Initially we take root of the Cartesian Tree as the
+             // first element of the input array. This can change
+             // according to the algorithm
+             int root = 0, last;
+
+             // Starting from the second element of the input array
+             // to the last on scan across the elements, adding them
+             // one at a time.
+             for (int i = 1; i <= n - 1; i++) {
+                 last = i - 1;
+                 rightchild[i] = -1;
+
+                 // Scan upward from the node's parent up to
+                 // the root of the tree until a node is found
+                 // whose value is greater than the current one
+                 // This is the same as Step 2 mentioned in the
+                 // algorithm
+                 while (arr[last] <= arr[i] && last != root)
+                     last = parent[last];
+
+                 // arr[i] is the largest element yet; make it
+                 // new root
+                 if (arr[last] <= arr[i]) {
+                     parent[root] = i;
+                     leftchild[i] = root;
+                     root = i;
+                 }
+
+                 // Just insert it
+                 else if (rightchild[last] == -1) {
+                     rightchild[last] = i;
+                     parent[i] = last;
+                     leftchild[i] = -1;
+                 }
+
+                 // Reconfigure links
+                 else {
+                     parent[rightchild[last]] = i;
+                     leftchild[i] = rightchild[last];
+                     rightchild[last] = i;
+                     parent[i] = last;
+                 }
+
+             }
+
+             // Since the root of the Cartesian Tree has no
+             // parent, so we assign it -1
+             parent[root] = -1;
+
+             return (buildCartesianTreeUtil(root, arr, parent,
+                 leftchild, rightchild));
+         }
+
+         static void memset(int[] arr, int value) {
+             for (int i = 0; i < arr.Length; i++) {
+                 arr[i] = value;
+             }
+
+         }
+     }
+
+     class Program {
+         static void Main(string[] args) {
+             //Backtracking.DoTask2();
+             
+             static void DoTask6() {
+                 string path6 = @"..\..\Data7.csv";
+                 int K = 1000;
+                 int[] arr = new int[1000];
+
+                 for (int i = 1; i < K; i++) {
+                     Stopwatch stopwatch = new Stopwatch();
+                     stopwatch.Start();
+                     for (int j = 1; j <= i; j++) {
+                         Random rnd = new Random();
+                         arr[j] = rnd.Next(1, 500);
+
+                         int n = arr.Length;
+                         
+                     
+                         GFG.Node root = GFG.buildCartesianTree(arr, n);
+
+                         /* Let us test the built tree by printing Inorder
+                         traversal */
+                         //Console.Write("Inorder traversal of the" +
+                         //              " constructed tree : \n");
+                         //GFG.printInorder(root);
+                         
+                     }
+                     stopwatch.Stop();
+                     string time = (stopwatch.ElapsedTicks).ToString();
+                     File.AppendAllText(path6, time + ";");
+                 }
+             }
+             
+             DoTask6();
+             
+         }
+     }
 }
